9ff1eb26af40efd8d1cc9d23e90eacd32f7dc72c
TomonagaNoboru <2110801h@stu.kobe-u.ac.jp>
2022-12-13 12:05:38 +0900

    kyoutu

---
+++ kadai20221213/gobi.js
@@ -0,0 +1,12 @@
+function f(n){
+    let cs = n.childNodes;
+    for(let i = 0; i < cs.length; i++){
+      let c = cs[i];
+      if(c.nodeType == Node.TEXT_NODE){
+        c.textContent = c.textContent.replace(/です。/g, "でござるよ。");
+      }
+      else{
+        f(c);
+      }
+    }
+  }
